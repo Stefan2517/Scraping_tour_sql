58e45b3e3ba0f862974fc266f4091b6e6f8ff2b5
Stefan2517 <117020443+Stefan2517@users.noreply.github.com>
2023-11-26 21:21:25 +0200

    OOP Concept

--- main.py
+++ main.py
@@ -7,66 +7,72 @@ URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-connection = sqlite3.connect("data.db")
+
+class Event:
+    def scrape(self, url):
+        """Scrape the page source from the URL"""
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
 
 
-def scrape(url):
-    """Scrape the page source from the URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["orice"]
+        return value
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["orice"]
-    return value
+class Email:
+    def send_email(self, message):
+        host = "smtp.gmail.com"
+        port = 465
+
+        username = "iojosiojo912@gmail.com"
+        password = "evtk pufr idhm oppa"
+
+        receiver = "stefiarko803@gmail.com"
+        my_context = ssl.create_default_context()
 
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
-
-    username = "iojosiojo912@gmail.com"
-    password = "evtk pufr idhm oppa"
-
-    receiver = "stefiarko803@gmail.com"
-    my_context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+        print("Sent an email!")
 
 
-    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-    print("Sent an email!")
+class Database:
+    def __init__(self, database_path):
+        self.connection = sqlite3.connect(database_path)
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
 
-
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
-
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute("SELECT band, date FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT band, date FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
 
 if __name__=="__main__":
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
 
         if extracted != "No upcoming tours":
-            row = read(extracted)
+            database = Database(database_path="data.db")
+            row = database.read(extracted)
             if not row:
-                store(extracted)
-                send_email(message="Hey, new event was found!")
+                database.store(extracted)
+                email = Email()
+                email.send_email(message="Hey, new event was found!")
         time.sleep(2)
 
